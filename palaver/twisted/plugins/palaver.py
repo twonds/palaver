e6dec01b270d8e41bdb3586f01e3e89c53ad6c59
Christopher Zorn <tofu@thetofu.com>
2008-09-24 16:13:32 +0000

    doing some moving around

--- palaver/twisted/plugins/palaver.py
+++
@@ -1,7 +0,0 @@
-from twisted.scripts.mktap import _tapHelper
- 
-Palaver = _tapHelper(
-                "Palaver",
-                "palaver.tap",
-                "A multi-user chat jabber component.",
-                "palaver")
